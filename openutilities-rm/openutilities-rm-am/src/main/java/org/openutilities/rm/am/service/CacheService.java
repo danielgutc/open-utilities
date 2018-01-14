f70c2e37e00b72998158c06bb630369e6a7fc4b9
Daniel Gutierrez <dani.gutierrez@gmail.com>
2018-01-14 16:31:03 +0100

    Add comments

--- openutilities-rm/openutilities-rm-am/src/main/java/org/openutilities/rm/am/service/CacheService.java
+++ openutilities-rm/openutilities-rm-am/src/main/java/org/openutilities/rm/am/service/CacheService.java
@@ -4,6 +4,9 @@ import com.hazelcast.core.HazelcastInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+/**
+ * Cache service connects with the global cache.
+ */
 @Service
 public class CacheService
 {
